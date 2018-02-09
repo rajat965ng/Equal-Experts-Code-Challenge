6e417cb492f95ad382c6ae3f73bc561d33820903
Rajat Nigam <rajatnigam89@gmail.com]>
2018-02-09 06:32:50 +0530

    updated

--- src/test/java/com/shopping/cart/AddProductTest.java
+++ src/test/java/com/shopping/cart/AddProductTest.java
@@ -13,6 +13,21 @@ public class AddProductTest {
     private Cart cart;
 
     @Test
+    public void should_contain_5_dove_soaps_when_add_5_dove_soap(){
+        cart = new Cart();
+        cart.addProduct(Product.DOVE,5);
+        assertEquals(5,cart.getQtyByProduct(Product.DOVE));
+    }
+
+
+    @Test
+    public void should_not_contain_5_dove_soaps_when_add_6_dove_soap(){
+        cart = new Cart();
+        cart.addProduct(Product.DOVE,6);
+        assertNotEquals(5,cart.getQtyByProduct(Product.DOVE));
+    }
+
+    @Test
     public void should_contain_5_dove_soaps_and_total_equal_to_199_point_95_when_unit_price_is_39_point_99(){
         cart = new Cart();
         cart.addProduct(Product.DOVE,5);
